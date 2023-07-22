ebda4ac88beae0b3700efabd19050e7ceb261bf9
Alax David Oliveira <127566072+AlaxDav1d@users.noreply.github.com>
2023-07-22 18:38:08 -0300

    Update cadastro.php

--- MVC-php/cadastro.php
+++ MVC-php/cadastro.php
@@ -5,27 +5,124 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Cadastro Usuario</title>
+        <style>
+            @keyframes animacao{
+                0%{left: 500px;}
+                10%{right: 800px;}
+                20%{top: 500px;}
+                100%{top: 500px;}
+            }
+
+            *{
+                padding: 0;
+                margin: 0;
+                box-sizing: border-box;
+            }
+
+            .tudo{
+                height: 100vh;
+                background-color: #051150; 
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                gap: 5vh;
+            }
+            form{
+                display: flex;
+                flex-direction: column;
+                gap: 10px;
+                height: 80vh;
+                width: 60vh;
+                color: aliceblue;
+                background-color: #121e5c;
+                border: double black;
+                border-radius: 30px;
+                padding: 20px;
+                transition: 1s;
+                box-shadow: -5px -10px 10px black;
+                font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+                /*fazer uma animação com box shadow rodando os quatro cantos*/
+            }
+            form:hover{
+                background-color: #051150;
+            }
+            input{
+                font-size: 15px;
+                padding: 10px;
+                border-radius: 5px;
+            }
+            button{
+                background-color: rgb(255, 255, 255);
+                height: 20px;
+            }
+            form button:hover{
+                background-color: rgb(159, 7, 7);
+            }
+            header{
+                background-color: #041a85;
+                width: 100%;
+                height: 10vh;
+                display: flex;
+                align-items: center;
+                padding-left: 20px;
+                color: #fff;
+                font-family: Georgia, 'Times New Roman', Times, serif;
+                transition: 600ms;
+            }
+            header:hover{
+                background-color: #051150;
+            }
+            .ponto{
+                background-color: #fff;
+                width: 2px;
+                height: 2px;
+                border-radius: 50px;
+                position: relative;
+                animation: animacao infinite;
+                animation-duration: 5s;
+            }
+            .flex{
+                display: flex;   
+            }
+        </style>
     </head>
     <body>
-        <form action="">
-            <h2>Realize aqui o seu Cadastro</h2>
-            <label for="nome">Nome</label>
-            <input type="text" name="nomeCompleto" id="nomeCompletoTxt" >
+        <div class="tudo">
+            <button disabled="disabled" class="ponto"></button>
+            <header>
+                <h2>Bem Vindo ao nosso Sistema!!</h2>
+            </header>
+            <div class="flex">
+                <button disabled="disabled" class="ponto"></button>
+                <form action="">
+                <h2>Realize aqui o seu Cadastro</h2>
+                    <label for="nome">Nome</label>
+                    <input type="text" name="nomeCompleto" id="nomeCompletoTxt" >
 
-            <label for="data">Data de Nascimento</label>
-            <input type="text" name="DataNascimento" id="dataNascTxt" >
+                    <label for="data">Data de Nascimento</label>
+                    <input type="text" name="DataNascimento" id="dataNascTxt" >
 
-            <label for="email">Email</label>
-            <input type="email" name="email" id="emailTxt" >
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="emailTxt" >
 
-            <label for="senha">Senha</label>
-            <input type="password" name="senha" id="senhaTxt" >
+                    <label for="senha">Senha</label>
+                    <input type="password" name="senha" id="senhaTxt" >
 
-            <label for="telefone">Telefone</label>
-            <input type="tel" name="telefone" id="telefoneTxt">
+                    <label for="telefone">Telefone</label>
+                    <input type="tel" name="telefone" id="telefoneTxt">
 
-            <button id="submitBtn"></button>
-        </form>
+                    <button id="submitBtn"></button>
+                </form>
+                <button disabled="disabled" class="ponto"></button>
+                <button disabled="disabled" class="ponto"></button>
+                <button disabled="disabled" class="ponto"></button>
+                <button disabled="disabled" class="ponto"></button>
+                <button disabled="disabled" class="ponto"></button>
+            </div>
+        </div>
+       
+        
+        
     </body>
         <script src="js/jquery-3.6.0.min.js" type="text/javascript"></script>
         <script type="text/javascript" charset="utf-8">
